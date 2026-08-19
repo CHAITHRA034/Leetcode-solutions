54a2e894c1c8c482c66016fd14af50305de06bf2
Chaithra S P <csp2uk@gmail.com>
2026-08-19 20:03:23 +0530

    Time: 2 ms (99.31%), Space: 44.2 MB (83.74%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1; 
+        
+        while(left < right){
+            // 1. Skip non-alphanumeric characters from the left
+            if (!Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            } 
+            // 2. Skip non-alphanumeric characters from the right
+            else if (!Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            } 
+            // 3. Compare characters after converting both to lowercase
+            else {
+                if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}
